63eb02a247d764b488ee39ff2b203363c1d4e526
liuisaac <80558145+liuisaac@users.noreply.github.com>
2024-09-14 03:37:48 -0400

    comit

--- polaris-frontend/src/app/layout.tsx
+++ polaris-frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Saira, Bebas_Neue } from "next/font/google";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -11,6 +12,20 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const saira = Saira({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: 'swap',
+  variable: "--font-saira",
+});
+
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: 'swap',
+  variable: "--font-bebas",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${saira.variable} ${bebas.variable} antialiased`}
       >
         {children}
       </body>
--- polaris-frontend/tailwind.config.ts
+++ polaris-frontend/tailwind.config.ts
@@ -1,19 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                saira: ["var(--font-saira)"],
+                bebas: ["var(--font-bebas)"],
+            },
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                "space-blue": "#031E3C",
+                "button-orange": "#FF7A00",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
